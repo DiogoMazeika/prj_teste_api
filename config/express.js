36d0123b53511740b1ae8fbccff57031ec825ce7
diogo_mazeika <mazeikadiogo@gmail.com>
2023-05-15 23:36:38 -0300

    initial commit

---
+++ config/express.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { json } from "body-parser";
+import { get } from "config";
+
+export default () => {
+  const app = express();
+
+  // SETANDO VARIÁVEIS DA APLICAÇÃO
+  app.set("port", process.env.PORT || get("server.port"));
+
+  // MIDDLEWARES
+  app.use(json());
+
+  return app;
+};
